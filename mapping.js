74f4fd7711659a5b17e28bf1cae93d3f1c99ba07
vonbutt <nick.von.buttlar@gmail.com>
2018-09-27 21:48:06 +0000

    completed mapping assignment

--- mapping.js
+++ mapping.js
@@ -1,9 +1,13 @@
 var input = [
-  { x: 3, y: 4 },
+  { x: 3, y: 4 } ,
   { x: 12, y: 5 },
   { x: 8, y: 15 }
 ];
-var result = input.map(/* Your code here */);
+
+var result = input.map(function(currentValue){
+  var z = Math.hypot(currentValue.x, currentValue.y);
+  return z;
+});
 
 console.log(result[0] === 5);
 console.log(result[1] === 13);
