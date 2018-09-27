cc5d9ec74bd09cfe5e82e0a36b02e4d46b32ffed
vonbutt <nick.von.buttlar@gmail.com>
2018-09-27 20:34:54 +0000

    found waldo

---
+++ callback_intro.js
@@ -0,0 +1,16 @@
+function findWaldo(arr, found) {
+  arr.forEach(function (name, currentPositionIndex) {
+    if (name === "Waldo") {
+      found(currentPositionIndex);
+    }
+  });
+}
+
+function actionWhenFound(indexPosition) {
+  console.log("Found Waldo at index: " + indexPosition + "!");
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+
+
