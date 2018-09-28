9e8bdd7abf6cb5218a29b1aabf297856c242978b
vonbutt <nick.von.buttlar@gmail.com>
2018-09-28 02:04:10 +0000

    changes to experiment.js and start closures.js assignment

--- experiment.js
+++ experiment.js
@@ -1,14 +1,29 @@
-// The second argument/parameter is expected to be a function
-function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
-    }
+// // The second argument/parameter is expected to be a function
+// function findWaldo(arr, found) {
+//   for (var i = 0; i < arr.length; i++) {
+//     if (arr[i] === "Waldo") {
+//       found(i);   // execute callback
+//     }
+//   }
+// }
+
+// function actionWhenFound(indexPosition) {
+//   console.log("Found him at index: " + indexPosition + "!");
+// }
+
+// findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+// *** CLOSURES ***
+
+var foo = (function() {
+  var x = 10;
+
+  var inner = function() {
+    console.log("Value of x is: " + x);
   }
-}
+  return inner;
+})(); // Notice the () at the end,
+      // which will immediately invoke the function,
+      // assigning the returned value (inner) to foo
 
-function actionWhenFound(indexPosition) {
-  console.log("Found him at index: " + indexPosition + "!");
-}
-
-findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+foo();
